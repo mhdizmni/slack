e22794c94b34bc7491ff594b9facd7b62093ca8a
mhdizmni <mr.mzamani77@gmail.com>
2024-08-27 06:50:09 +0330

    feat(auth): auth forms

--- src/features/auth/components/sign-in-form.tsx
+++ src/features/auth/components/sign-in-form.tsx
@@ -1,63 +1,122 @@
 "use client"
 
-import { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useState, useTransition } from "react";
 
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/loaders/spinner";
+import { Notice } from "@/components/notice";
+import { Dots } from "@/components/loaders/dots";
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { SignInFlow } from "../types";
+import { cn } from "@/lib/utils";
 
 interface SignInFormProps {
     setState: (state: SignInFlow) => void;
 }
 
 export const SignInForm = ({ setState }: SignInFormProps) => {
+    const [isPending, setTransition] = useTransition();
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const [error, setError] = useState<string>("");
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setError("");
+        
+        setTransition(async () => {
+            await signIn("password", { email, password, flow: "signIn" })
+            .catch(() => {
+                setError("Invalid email or password");
+            });
+        })
+    }
+
+    const onProviderSignIn = (value: "google" | "github") => {
+        setError("");
+        setTransition(async () => {
+            await signIn(value);
+        })
+    }
+
     return (
-        <div className="flex flex-col items-center justify-center gap-4 w-full">
+        <div className={cn(
+            "flex flex-col items-center gap-4 w-full",
+            isPending && "animate-pulse"
+        )}>
             <h1 className="font-bold text-2xl">Sign in to Slack</h1>
             <p className="text-xs text-muted-foreground">We suggest using the email address you use at work.</p>
             <div className="flex flex-col gap-1 w-full">
                 <Button
                     variant="outline"
                     className="relative"
+                    onClick={() => onProviderSignIn("google")}
+                    disabled={isPending}
                 >
-                    <FcGoogle className="size-5 absolute left-2.5" />
-                    Sign In With Google
+                    <div className="absolute left-2.5">
+                        {isPending ? (
+                            <Spinner className="size-5" />
+                        ) : (
+                            <FcGoogle className="size-5" />
+                        )}
+                    </div>
+                    Sign In with Google
                 </Button>
                 <Button
                     variant="outline"
                     className="relative"
+                    onClick={() => onProviderSignIn("github")}
+                    disabled={isPending}
                 >
-                    <FaGithub className="size-5 absolute left-2.5" />
-                    Sign In With Github
+                    <div className="absolute left-2.5">
+                        {isPending ? (
+                            <Spinner className="size-5" />
+                        ) : (
+                            <FaGithub className="size-5" />
+                        )}
+                    </div>
+                    Sign In with Github
                 </Button>
             </div>
             <Separator />
-            <form className="space-y-2 w-full">
+            <form className="space-y-2 w-full" onSubmit={onPasswordSignIn}>
+                {!!error && (
+                    <Notice type="error">
+                        {error}
+                    </Notice>
+                )}
                 <Input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    disabled={isPending}
+                    required
                 />
                 <Input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    disabled={isPending}
+                    required
                 />
                 <Button
                     type="submit"
                     className="w-full"
+                    disabled={isPending}
                 >
-                    Sign In
+                    {isPending ? <Dots /> : "Sign In"}
                 </Button>
             </form>
             <div className="text-xs">
--- src/features/auth/components/sign-up-form.tsx
+++ src/features/auth/components/sign-up-form.tsx
@@ -1,70 +1,136 @@
 "use client"
 
-import { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useState, useTransition } from "react";
 
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/loaders/spinner";
+import { Notice } from "@/components/notice";
+import { Dots } from "@/components/loaders/dots";
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { SignInFlow } from "../types";
+import { cn } from "@/lib/utils";
 
 interface SignUpFormProps {
     setState: (state: SignInFlow) => void;
 }
 
 export const SignUpForm = ({ setState }: SignUpFormProps) => {
+    const [isPending, setTransition] = useTransition();
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
+    
+    const [error, setError] = useState<string>("");
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        setError("");
+
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+        
+        setTransition(async () => {
+            await signIn("password", { email, password, flow: "signUp" })
+            .catch(() => {
+                setError("Invalid email or password");
+            });
+        })
+    }
+
+    const onProviderSignIn = (value: "google" | "github") => {
+        setError("");
+        setTransition(async () => {
+            await signIn(value);
+        })
+    }
 
     return (
-        <div className="flex flex-col items-center gap-4 w-full">
+        <div className={cn(
+            "flex flex-col items-center gap-4 w-full",
+            isPending && "animate-pulse"
+        )}>
             <h1 className="font-bold text-2xl">Sign up to Slack</h1>
             <p className="text-xs text-muted-foreground">We suggest using the email address you use at work.</p>
             <div className="flex flex-col gap-1 w-full">
                 <Button
                     variant="outline"
                     className="relative"
+                    onClick={() => onProviderSignIn("google")}
+                    disabled={isPending}
                 >
-                    <FcGoogle className="size-5 absolute left-2.5" />
-                    Sign In With Google
+                    <div className="absolute left-2.5">
+                        {isPending ? (
+                            <Spinner className="size-5" />
+                        ) : (
+                            <FcGoogle className="size-5" />
+                        )}
+                    </div>
+                    Continue with Google
                 </Button>
                 <Button
                     variant="outline"
                     className="relative"
+                    onClick={() => onProviderSignIn("github")}
+                    disabled={isPending}
                 >
-                    <FaGithub className="size-5 absolute left-2.5" />
-                    Sign In With Github
+                    <div className="absolute left-2.5">
+                        {isPending ? (
+                            <Spinner className="size-5" />
+                        ) : (
+                            <FaGithub className="size-5" />
+                        )}
+                    </div>
+                    Continue with Github
                 </Button>
             </div>
             <Separator />
-            <form className="space-y-2 w-full">
+            <form className="space-y-2 w-full" onSubmit={onPasswordSignIn}>
+                {!!error && (
+                    <Notice type="error">
+                        {error}
+                    </Notice>
+                )}
                 <Input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    disabled={isPending}
+                    required
                 />
                 <Input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    disabled={isPending}
+                    required
                 />
                 <Input
                     type="password"
                     placeholder="Confirm Password"
                     value={confirmPassword}
                     onChange={(e) => setConfirmPassword(e.target.value)}
+                    disabled={isPending}
+                    required
                 />
                 <Button
                     type="submit"
                     className="w-full"
+                    disabled={isPending}
                 >
-                    Sign Up
+                    {isPending ? <Dots /> : "Sign Up"}
                 </Button>
             </form>
             <div className="text-xs">
