9a72ad8f64b7cf0b7b7e3e29e0d53625a1fb562d
mhdizmni <mr.mzamani77@gmail.com>
2024-08-28 01:15:33 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
 "use client"
 
-import { useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { useAuthActions } from "@convex-dev/auth/react";
-import { Button } from "@/components/ui/button";
+import { useEffect, useMemo } from "react";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useRouter } from "next/navigation";
+
 import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const { data, isLoading } = useGetWorkspaces();
+
+  const router = useRouter();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data])
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [isLoading, workspaceId, open, setOpen, router])
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <UserButton />
