a9ec27f0776cf52cbc534332c4ddfd009af33c66
mhdizmni <mr.mzamani77@gmail.com>
2024-08-27 06:50:44 +0330

    lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-
+  const { signOut } = useAuthActions();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       main page
+      <Button onClick={() => signOut()}>out</Button>
     </main>
   );
 }
--- src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = () => {
-    return (
-        <>page</>
-    );
-}
- 
-export default Page;
