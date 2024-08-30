c693776f30eed57df95373e0d22f6f9cb7a4df38
mhdizmni <mr.mzamani77@gmail.com>
2024-08-31 01:15:01 +0330

    Update layout.tsx

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -13,7 +13,12 @@ export default function WorkspaceLayout({
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
+        <div className="bg-[#83388a] w-full h-full">
+          <div className="bg-background rounded h-[calc(100%-4px)] w-[calc(100%-4px)]">
+
         {children}
+          </div>
+        </div>
       </div>
     </div>
   );
