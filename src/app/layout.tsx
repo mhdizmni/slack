e6bfdbf6d85c11efe387cd697f9cf41b20cbbc4e
mhdizmni <mr.mzamani77@gmail.com>
2024-08-31 06:35:06 +0330

    JotaiProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { JotaiProvider } from "@/components/jotai-provider";
 
 import { Modals } from "@/components/modals";
 import { Toaster } from "@/components/ui/sonner";
@@ -26,9 +27,11 @@ export default function RootLayout({
       <html lang="en">
         <body className={nunito.className}>
           <ConvexClientProvider>
-            <Modals />
-            <Toaster />
-            {children}
+            <JotaiProvider>
+              <Modals />
+              <Toaster />
+              {children}
+            </JotaiProvider>
           </ConvexClientProvider>
         </body>
       </html>
---
+++ src/components/jotai-provider.tsx
@@ -0,0 +1,12 @@
+import { Provider } from "jotai"
+
+interface JotaiProviderProps {
+    children: React.ReactNode
+}
+export const JotaiProvider = ({ children }: JotaiProviderProps) => {
+    return (
+        <Provider>
+            {children}
+        </Provider>
+    )
+}
